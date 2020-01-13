87a13dccd636d05fd1cff95eb53250c702b4f0b8
Michelle <wong.mich.c@gmail.com>
2020-01-13 16:10:03 +1100

    adding user collection

--- models/user.js
+++ models/user.js
@@ -2,7 +2,14 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({username: String, password: String, isAdmin: Boolean, email: String});
+const userSchema = new Schema({
+  username: String, 
+  password: String, 
+  isAdmin: Boolean, 
+  email: String},
+  {
+    collection: 'users'
+});
 
 const User = mongoose.model("User", userSchema);
 
