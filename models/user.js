2e33f80c1ba0b07e86faf50871451340a4bda3e9
Pankaj Pawar <pankaj.pawar1986@yahoo.com.au>
2020-01-13 15:20:00 +1100

    Added validation to user model

--- models/user.js
+++ models/user.js
@@ -2,7 +2,35 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({username: String, password: String, isAdmin: Boolean, email: String});
+const userSchema = new Schema({
+  username: 
+  {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+  password:
+  {
+    type: String,
+    required: true
+  },
+  isAdmin: 
+  {
+    type: Boolean,
+    required: true
+  },
+  email:
+  {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(v);
+      },
+      message: props => `${props.value} is not a valid email address!`
+    }
+  }
+  
+});
 
 const User = mongoose.model("User", userSchema);
 
