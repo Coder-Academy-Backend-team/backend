5edb56d3984ae43b8e21f836c8968a8147c8637e
techspeterson <tpeterson.engsci@gmail.com>
2020-01-13 14:35:55 +1100

    start scripts

--- routes/cafe-routes.js
+++ routes/cafe-routes.js
@@ -6,10 +6,9 @@ const { index, create } = require("../controllers/review-controller");
 router.get('/', index);
 router.post('/create', create);
 
- module.exports = router;
 
 //add cafe
-router.post('/create', (req, res)=> {
+router.post('/create', (req, res) => {
     const { name, location, rating, description, link } = req.body.name;
 
     const newReview = new Review({
@@ -21,15 +20,15 @@ router.post('/create', (req, res)=> {
     })
 
     newReview.save()
-    .then(()=>res.json('Review added!'))
-    .catch(err=> res.status(400).json('Error: ' + err));
+        .then(() => res.json('Review added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
 // router.delete();
 
 
 // find all cafe
-router
+
 // find one cafe
 
-//
+module.exports = router;
