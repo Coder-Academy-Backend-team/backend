1a03c7d6f86cc112e2585926bae82e9d12dd991d
techspeterson <tpeterson.engsci@gmail.com>
2020-01-13 14:07:44 +1100

    some basic routes

--- routes/review-routes.js
+++ routes/review-routes.js
@@ -6,32 +6,42 @@ const { index, create } = require("../controllers/review-controller");
 router.get('/', index);
 router.post('/create', create);
 
- module.exports = router;
+module.exports = router;
+
+
+
 //add review
-router.post('/add', (req, res)=> {
-    const name = req.body.name;
-    const location = req.body.location;
-    const rating = req.body.rating;
-    const description = req.body.description;
-    const link = req.body.link;
+router.post('/add', (req, res) => {
+    const { coffeeType, milkType, photo, rating, comment } = req.body;
 
     const newReview = new Review({
-        name,
-        location,
+        coffeeType,
+        milkType,
+        photo,
         rating,
-        description,
-        link,
-    })
+        comment
+    });
 
     newReview.save()
-    .then(()=>res.json('Review added!'))
-    .catch(err=> res.status(400).json('Error: ' + err));
+        .then(() => res.json('Review added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
 })
+
 //find by coffee
-
-
+router.get("/search/coffee/:type", (req, res) => {
+    const { type } = req.params;
+    Reviews.find({ coffeeType: type })
+        .then((reviews) => {
+            // do this
+        });
+});
 
 //find by cafe
+router.get("/search/cafe/:name", (req, res) => {
+    const { name } = req.params;
+    // find reviews by cafe
+});
 
-
-
+router.delete("/delete/:id", (req, res) => {
+    const { id } = req.params;
+})
